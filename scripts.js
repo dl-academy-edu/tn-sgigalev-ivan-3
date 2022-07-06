e54327e43a65c774af370898f14915b7fd82b688
Birlogich <vangogimomo@gmail.com>
2022-07-06 18:16:28 +0400

    06.07.2022

--- scripts.js
+++ scripts.js
@@ -4,10 +4,6 @@ var popup = document.querySelector('.popup__sign');
 var closePopup = document.querySelector('.popup__close');
 var input = popup.querySelector('input');
 
-console.log(popup);
-
-console.log(popupLink);
-
 popupLink.addEventListener('click', function () {
 	popup.classList.add('popup__sign_open');
 	input.focus();
@@ -21,7 +17,6 @@ closePopup.addEventListener('click', function () {
 
 
 window.addEventListener('keydown', function (event) {
-	console.log(event.code);
 	if (event.code === "Escape" && popup.classList.contains('popup__sign_open')) {
 		popup.classList.remove('popup__sign_open');
 		popupLink.focus();
@@ -37,10 +32,6 @@ var popup_reg = document.querySelector('.popup__register');
 var closePopup_reg = document.querySelector('.popup__close_reg');
 var input_reg = popup.querySelector('input');
 
-console.log(popup_reg);
-
-console.log(popupLink_reg);
-
 popupLink_reg.addEventListener('click', function () {
 	popup_reg.classList.add('popup__register_open');
 	input.focus();
@@ -52,9 +43,7 @@ closePopup_reg.addEventListener('click', function () {
 	document.body.style.overflow = 'visible'
 })
 
-
 window.addEventListener('keydown', function (event) {
-	console.log(event.code);
 	if (event.code === "Escape" && popup_reg.classList.contains('popup__register_open')) {
 		popup_reg.classList.remove('popup__register_open');
 		popupLink_reg.focus();
@@ -63,17 +52,12 @@ window.addEventListener('keydown', function (event) {
 })
 
 
-
 //popUP mes
 
 var popupLink_mes = document.querySelector('.popup__link_message');
 var popup_mes = document.querySelector('.popup__message');
 var closePopup_mes = document.querySelector('.popup__close_mes');
 var input_mes = popup.querySelector('input');
-
-console.log(popup_mes);
-
-console.log(popupLink_mes);
 
 popupLink_mes.addEventListener('click', function () {
 	popup_mes.classList.add('popup__message_open');
@@ -88,7 +72,6 @@ closePopup_mes.addEventListener('click', function () {
 
 
 window.addEventListener('keydown', function (event) {
-	console.log(event.code);
 	if (event.code === "Escape" && popup_mes.classList.contains('popup__message_open')) {
 		popup_mes.classList.remove('popup__message_open');
 		popupLink_mes.focus();
@@ -99,15 +82,9 @@ window.addEventListener('keydown', function (event) {
 
 //burger menu
 
-
 var burger = document.querySelector('.header__mobile');
 var burgerButton = document.querySelector('.burger__button');
 var closeBurger = document.querySelector('.burger__close');
-
-
-console.log(burger);
-
-console.log(burgerButton);
 
 burgerButton.addEventListener('click', function () {
 	burger.classList.add('header__mobile_open');
@@ -119,7 +96,6 @@ closeBurger.addEventListener('click', function () {
 })
 
 window.addEventListener('keydown', function (event) {
-	console.log(event.code);
 	if (event.code === "Escape" && burger.classList.contains('header__mobile_open')) {
 		burger.classList.remove('header__mobile_open');
 	}
@@ -135,8 +111,6 @@ let buttonToTop = document.querySelector('.scroll__button');
 window.addEventListener('scroll', trackScroll);
 
 buttonToTop.addEventListener('click', backToTop);
-
-buttonToTop.log(buttonToTop);
 
 function trackScroll() {
 	var scrolled = window.pageYOffset;
@@ -156,3 +130,58 @@ function backToTop() {
 		setTimeout(backToTop, 15);
 	}
 }
+
+//Validation Form
+
+const formSign = document.forms[0];
+
+formSign.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const inputSign = formSign.querySelectorAll('input');
+
+	const dataForServer = {};
+
+	for (input of inputSign) {
+		dataForServer[input.name] = input.value;
+	}
+
+	console.log(dataForServer);
+});
+
+
+//Form-register
+
+const formRegister = document.forms[1];
+
+let mailCheck = /^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/;
+let phoneCheck = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
+
+
+formRegister.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const agreement = formRegister.querySelector(`[name ="agreement"]`)
+
+	if (!agreement.checked) {
+		alert('Agree with me!')
+		return
+	}
+
+	const textsRegister = formRegister.querySelectorAll(`[type='text']`)
+	const isTextCorrect = mailCheck.test(textsRegister.value);
+	console.log(isTextCorrect);
+
+	const inputRegister = formRegister.querySelectorAll('input');
+
+	const dataForServer = {};
+
+	for (input of inputRegister) {
+		dataForServer[input.name] = input.checked
+			? input.checked
+			: input.value;
+	}
+
+
+	console.log(dataForServer);
+});
--- src/components/header/script.js
+++
@@ -1,4 +0,0 @@
-
-let scroll = document.querySelector('.scroll__image');
-
-console.log(scroll);
