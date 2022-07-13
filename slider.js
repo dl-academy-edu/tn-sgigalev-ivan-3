bf03891e3317b91931a417424dd88ca34f5d2057
Birlogich <vangogimomo@gmail.com>
2022-07-13 14:30:38 +0400

    13.07.2022

--- slider.js
+++ slider.js
@@ -2,7 +2,6 @@ const sliderWrapper = document.querySelector('.slider__wrapper');
 const innerSliderWrapper = document.querySelector('.slider__inner-wrapper');
 const slides = document.querySelectorAll('.slider__slide');
 const pagination = document.querySelector('.slider__pagination');
-
 const buttonPrev = document.querySelector('.arrow_prev');
 const buttonNext = document.querySelector('.arrow_next');
 const slidesAmount = innerSliderWrapper.childElementCount;
@@ -26,6 +25,7 @@ const changeActiveSlide = (direction) => {
 		case 'next':
 			if (activeSlide < slidesAmount) {
 				innerSliderWrapper.style.marginLeft = `${currentML - slideWidth}px`;
+				changeActiveDot(activeSlide);
 				activeSlide++;
 				buttonPrev.removeAttribute('disabled');
 			}
@@ -40,6 +40,7 @@ const changeActiveSlide = (direction) => {
 			if (activeSlide !== 1) {
 				innerSliderWrapper.style.marginLeft = `${currentML + slideWidth}px`;
 				activeSlide--;
+				changeActiveDot(activeSlide - 1);
 				buttonNext.removeAttribute('disabled');
 			}
 
@@ -57,6 +58,14 @@ const changeActiveDot = (index) => {
 	const activeDot = document.querySelector('.slider__dot_active');
 	activeDot.classList.remove('slider__dot_active');
 	pagination.children[index].classList.add('slider__dot_active');
+	if (index === 0) {
+		buttonPrev.setAttribute('disabled', true);
+		buttonNext.removeAttribute('disabled');
+	}
+	if (index === slidesAmount) {
+		buttonNext.setAttribute('disabled', true);
+		buttonPrev.removeAttribute('disabled');
+	}
 }
 
 for (let i = 1; i <= slidesAmount; i++) {
@@ -70,7 +79,7 @@ for (let i = 1; i <= slidesAmount; i++) {
 	dot.classList.add('slider__dot')
 	const currentSlideIndex = i - 1;
 	dot.addEventListener('click', () => {
-		innerSliderWrapper.style.marginLeft = `- ${currentSlideIndex * slideWidth}px`
+		innerSliderWrapper.style.marginLeft = `-${currentSlideIndex * slideWidth}px`
 		changeActiveDot(currentSlideIndex);
 	})
 
